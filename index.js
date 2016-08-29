680062984a11d2f991cc5e5e4e3baf19ef43d93a
Jason Ayre <jayre@peer60.com>
2016-08-29 16:28:08 -0600

    trying to register module correctly

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 (function(angular, undefined) {
 'use strict';
 // Preload some angular stuff
-  var KoreJSModule = angular.module('korejs', []);
   var defs = require('./defs');
-  KoreJSModule.constant('Kore', defs);
+  return angular.module('korejs', ["ng"]).constant('Kore', defs);
+
+  // console.log(defs);
+  // KoreJSModule.constant('Kore', defs);
 });
