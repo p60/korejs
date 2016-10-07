8e84631cd89a5cff483edc43b5ef7e4ba04cd405
Jason Ayre <jayre@peer60.com>
2016-10-07 12:50:37 -0600

    fix exports

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 (function(exports, angular){
   'use strict';
   var defs = require('./defs');
+  exports.Kore = defs;
   angular.module('korejs', ["ng"]).constant('Kore', defs);
 })(module.exports, window.angular);
